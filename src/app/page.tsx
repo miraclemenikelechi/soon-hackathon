57cfb5dca6770311b2bfe5363740509ea6e6a503
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-11-20 09:58:24 +0100

    footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { useHydration } from "@/hooks/hydration";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+// import { useHydration } from "@/hooks/hydration";
+// import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function Home() {
-	const isHydrated = useHydration();
-
 	return (
 		<main className="flex items-center justify-center">
-			<div className="border hover:border-slate-900 rounded">
-				{isHydrated ? <WalletMultiButton style={{}} /> : null}
-			</div>
+			{/* <div className="border hover:border-slate-900 rounded">
+				
+			</div> */}
 			<h1 className="text-3xl underline">hello world</h1>
 		</main>
 	);
